9cfcbe03d1cb23d623ee96e47b4a0402ed8c921c
dinhhoanganh2020@gmail.com <dinhhoanganh2020@gmail.com>
2023-02-03 21:56:33 +0700

    video 19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -10,12 +10,23 @@ class MyComponent extends React.Component {
         address: 'Hoi Dan IT',
         age: 26
     };
+
+    handleClick(event) {
+        console.log("Click Me");
+        console.log("My name is ", this.state.name);
+    }
+
+    handleOnMouseOver(event) {
+        console.log(event.pageX);
+    }
     
     //JSX
     render() {
         return(
             <div>
                 My name is {this.state.name} and I'm from {this.state.address}
+                <button onMouseOver={this.handleOnMouseOver}>Hover Me</button>
+                <button onClick={this.handleClick}>Click Me</button>
             </div>
         );
     };
