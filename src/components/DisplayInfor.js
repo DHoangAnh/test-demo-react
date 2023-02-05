d0a67c79a70c643542bf6029b1e4bf3e4ee71f66
dinhhoanganh2020@gmail.com <dinhhoanganh2020@gmail.com>
2023-02-06 01:12:28 +0700

    video 33

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -3,9 +3,29 @@ import './DisplayInfor.scss';
 import logo from './../logo.svg';
 
 class DisplayInfor extends React.Component {
+    constructor(props) {
+        console.log('>>> call constructor: 1');
+        super(props);
+        // babel compiler
+        this.state = {
+            isShowListUser: true
+        }
+    }
 
-    state = {
-        isShowListUser: true
+    componentDidMount() {
+        console.log('>>> call me component did mount');
+        setTimeout(() => {
+            document.title = 'Hoi Dan IT && Eric';
+        }, 3000);
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('>>> call me component did update', this.props, prevProps);
+        if (this.props.listUsers !== prevProps.listUsers) {
+            if (this.props.listUsers.length === 5) {
+                alert('You got 5 user');
+            }
+        }
     }
 
     handleShowHide = () => {
@@ -15,6 +35,7 @@ class DisplayInfor extends React.Component {
     }
 
     render() {
+        console.log('>>> call me render');
         // destructuring array/object
         const { listUsers } = this.props;
         // props => properties
