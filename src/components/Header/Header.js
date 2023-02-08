ff5e142e64a470a8d8d10587f3fec632c6929425
dinhhoanganh2020@gmail.com <dinhhoanganh2020@gmail.com>
2023-02-08 23:52:28 +0700

    video 53

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,11 +17,13 @@ const Header = () => {
             <NavLink className="nav-link" to="/admins">Admin</NavLink>
           </Nav>
           <Nav>
-            <NavDropdown title="Settings" id="basic-nav-dropdown">
+            <button className="btn-login">Log In</button>
+            <button className="btn-signup">Sign Up</button>
+            {/* <NavDropdown title="Settings" id="basic-nav-dropdown">
                 <NavDropdown.Item >Log In</NavDropdown.Item>
                 <NavDropdown.Item >Log Out</NavDropdown.Item>
                 <NavDropdown.Item >Profile</NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
